e6a444b713648b47fb7118db68da7b249230f92b
Pratheesh <pratheesh1218@gmail.com>
2023-07-31 14:56:30 +0530

    Callback sample

---
+++ JS Concepts/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+function myDisplayer(some){
+    document.getElementById("demo").innerHTML = some;
+    
+}
+
+function myFirst(){
+    myDisplayer("Hello");
+};
+
+
+function mySecond(){
+    myDisplayer("GoodBye");
+}
+
+mySecond();
+myFirst();
+
+
+});
