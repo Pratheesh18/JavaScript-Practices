1f69ec1cb2054409c40f939ba4f8dadff378f4c5
Pratheesh <pratheesh1218@gmail.com>
2023-08-01 21:37:33 +0530

    new file added

--- JS Concepts/index.js
+++ JS Concepts/index.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-function myDisplayer(some){
-    document.getElementById("demo").innerHTML = some;
-    
-}
+    function myDisplayer(something){
+        document.getElementById("demo").innerHTML = something;
+    }
 
-function myFirst(){
-    myDisplayer("Hello");
-};
+    function myCalculator(num1 , num2,myCallback){
+        let sum = sum1 + sum2;
+        myCallback(sum);
+    }
+
+    myCalculator(5,5,myDisplayer)
 
 
-function mySecond(){
-    myDisplayer("GoodBye");
-}
-
-mySecond();
-myFirst();
 
 
 });
