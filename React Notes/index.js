3574fd91c8f18db7ff3599d97145ec40bf132857
Pratheesh <pratheesh1218@gmail.com>
2023-09-10 20:05:55 +0530

    adding react js notes

---
+++ React Notes/index.js
@@ -0,0 +1,9 @@
+//This is function declaration and this can be immediately exported with export default or export
+export default function App(){
+    return <div> hello React </div>;
+};
+
+//This is the arrow function , and 
+export const App = () => {
+    return <div> Hello react </div>;
+}
