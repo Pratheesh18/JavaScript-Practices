663aa1913b5946459745af6f486ab1cc81e705e8
Pratheesh <pratheesh1218@gmail.com>
2023-10-04 17:44:25 +0530

    added some js notes

---
+++ Javascript Design patterns/Function.js
@@ -0,0 +1,14 @@
+{
+    let message = "Hello";
+    console.log(message);
+}
+
+// console.log(message);
+{
+    let message = "Goodbye";
+    console.log(message);
+}
+// In Javascript,every running function , code block , and the script as a whole have an internal associated object known
+// lexical environment
+// The Lexical environment object consists of two parts
+// 1 . Environemnt record - an object
